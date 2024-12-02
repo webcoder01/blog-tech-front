d468ab98b343a4f9a3ffdd6e160c57ca7feefb30
Antoine Raymond <antoine.raymond@teksial.com>
2024-12-02 16:06:11 +0100

    feat: create client to get posts from api

---
+++ src/infrastructure/request/ApiClient.ts
@@ -0,0 +1,29 @@
+export interface ApiClientInterface {
+  getCollectionByPage<T>(url: string, page: number): Promise<T>;
+}
+
+export class ApiClient implements ApiClientInterface {
+  async getCollectionByPage<T>(url: string, page: number): Promise<T> {
+    const uri = this.getUriWithPagination(url, page);
+    const response = await fetch(uri, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+      },
+    });
+
+    if (response.ok) {
+      const responseData: { data: T } = await response.json();
+
+      return responseData.data;
+    }
+
+    throw new Error("Api error");
+  }
+
+  private getUriWithPagination(url: string, page: number): string {
+    return `${process.env.API_URL}${url}?pagination[page]=${page}`;
+  }
+}
---
+++ tests/infrastructure/request/ApiClient.spec.ts
@@ -0,0 +1,44 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+import { ApiClient } from "@/infrastructure/request/ApiClient";
+
+global.fetch = vi.fn().mockResolvedValue({
+  ok: true,
+  json: vi.fn().mockResolvedValue({ data: { key: "value" } }),
+});
+
+describe("ApiClient", () => {
+  const originalEnv = { ...process.env };
+  beforeEach(() => {
+    process.env.API_URL = "http://localhost:1337/api";
+    process.env.API_TOKEN = "api_token";
+  });
+
+  afterEach(() => {
+    process.env = originalEnv;
+    vi.clearAllMocks();
+  });
+
+  test("getCollectionByPage() should calls fetch with GET method and expected headers", async () => {
+    const apiClient = new ApiClient();
+    await apiClient.getCollectionByPage("/resources", 1);
+
+    expect(fetch).toHaveBeenCalledWith(
+      "http://localhost:1337/api/resources?pagination[page]=1",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer api_token",
+        },
+      },
+    );
+  });
+
+  test("getCollectionByPage() should returns data when there api response status is OK", async () => {
+    const apiClient = new ApiClient();
+    const data = await apiClient.getCollectionByPage("/resources", 1);
+
+    expect(data).toStrictEqual({ key: "value" });
+  });
+});
